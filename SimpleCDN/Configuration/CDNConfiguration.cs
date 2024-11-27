dc48eced3a40450af81dc6257c5e9c09307cb1d6
Jonathan Bout <jonathanbout01@gmail.com>
2024-11-27 23:53:44 +0100

    version number

--- SimpleCDN/Configuration/CDNConfiguration.cs
+++ SimpleCDN/Configuration/CDNConfiguration.cs
@@ -1,7 +1,27 @@
-﻿namespace SimpleCDN.Configuration
+﻿using System.Diagnostics;
+using System.Reflection;
+
+namespace SimpleCDN.Configuration
 {
 	public class CDNConfiguration
 	{
+		private static string? _version;
+		public static string Version
+		{
+			get
+			{
+				if (_version is not null) return _version;
+
+				var parentDir = new FileInfo(AppContext.BaseDirectory).Directory;
+
+				var versionFile = new FileInfo(Path.Combine(parentDir?.FullName ?? "/", ".version"));
+
+				if (!versionFile.Exists) return _version = "0.0.1-dev";
+
+				return _version = File.ReadAllText(versionFile.FullName);
+			}
+		}
+
 		/// <summary>
 		/// The data root path
 		/// </summary>
@@ -11,7 +31,7 @@
 		/// </summary>
 		public uint MaxMemoryCacheSize { get; set; } = 500;
 
-		public string Footer { get; set; } = """<a href="https://github.com/jonathanbout/simplecdn">Powered by SimpleCDN</a>""";
+		public string Footer { get; set; } = $"""<a href="https://github.com/jonathanbout/simplecdn">Powered by SimpleCDN v{Version}</a>""";
 		public string PageTitle { get; set; } = "SimpleCDN";
 	}
 }
