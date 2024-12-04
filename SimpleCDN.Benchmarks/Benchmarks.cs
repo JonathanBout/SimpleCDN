35fd2cf686f6350af865098812dc1d294524403e
Jonathan Bout <jonathanbout01@gmail.com>
2024-12-05 00:05:04 +0100

    improve benchmarks

--- SimpleCDN.Benchmarks/Benchmarks.cs
+++ SimpleCDN.Benchmarks/Benchmarks.cs
@@ -1,4 +1,5 @@
 ﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
@@ -9,35 +10,27 @@ using System.Threading.Tasks;
 namespace SimpleCDN.Benchmarks
 {
 	[MemoryDiagnoser(false)]
+	//[SimpleJob(RuntimeMoniker.NativeAot90)]
+	[SimpleJob(RuntimeMoniker.Net90)]
 	public class Benchmarks
 	{
-		public IEnumerable<char[]> Paths =>
+		public static IEnumerable<char[]> Paths =>
 		[
-			"/".ToCharArray(),
-			"/../".ToCharArray(),
-			"/data/../".ToCharArray(),
 			"/test.txt".ToCharArray(),
-			"/data/../test.txt".ToCharArray(),
-			"/data/test.json".ToCharArray(),
-			"/data/../data/test.json".ToCharArray(),
-			"/data/./../data/test.json".ToCharArray(),
-			"/data/data/../../data/test.json".ToCharArray(),
-			"/data/data/../../data/../data/test.json".ToCharArray(),
-			"/data/data/../../data/../data/../data/test.json".ToCharArray(),
 			"/data/data/.././data/../../../data/../data/test.json".ToCharArray(),
+			"/data/data/../../data/../data/../data/../data/./../data/data/../data/../data/../data/./../data/test/data/../../test/data/../../data/../data/test.json".ToCharArray(),
 		];
 
-		[ParamsSource(nameof(Paths))]
-		public char[] Path { get; set; } = [];
-
-		const int NormalizationBenchmarkIterationsPerInvoke = 100;
+		const int NormalizationBenchmarkIterationsPerInvoke = 50;
 
 		[Benchmark(OperationsPerInvoke = NormalizationBenchmarkIterationsPerInvoke)]
-		public void NormalizationBenchmark()
+		[ArgumentsSource(nameof(Paths))]
+		public void NormalizationBenchmark(char[] path)
 		{
-			var span = Path.AsSpan();
 			for (var i = 0; i < NormalizationBenchmarkIterationsPerInvoke; i++)
 			{
+				var copy = path.ToArray();
+				var span = copy.AsSpan();
 				Helpers.Extensions.Normalize(ref span);
 			}
 		}
