02f748e4a6f52b4b6908b3f51ec1b61d0be04fed
Jonathan Bout <jonathanbout01@gmail.com>
2025-02-19 10:52:49 +0100

    Unit test for naughty strings (#134)

--- tests/unit/SpanExtensionsTests.cs
+++ tests/unit/SpanExtensionsTests.cs
@@ -1,4 +1,6 @@
-﻿using SimpleCDN.Helpers;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using NaughtyStrings;
+using SimpleCDN.Helpers;
 
 namespace SimpleCDN.Tests.Unit
 {
@@ -32,6 +34,33 @@ namespace SimpleCDN.Tests.Unit
 				.ToList();
 
 			Assert.That(resultStrings, Has.None.Contain('/'));
+		}
+
+		[Test]
+		public void SplitPathSegments_NaughthyStrings_StillWorks()
+		{
+			for (int i = 0; i < TheNaughtyStrings.All.Count - 2; i++)
+			{
+				if (TheNaughtyStrings.All[i].Contains('/') || TheNaughtyStrings.All[i + 1].Contains('/')
+				 || TheNaughtyStrings.All[i].Contains('\\') || TheNaughtyStrings.All[i + 1].Contains('\\')
+				 || string.IsNullOrWhiteSpace(TheNaughtyStrings.All[i]) || string.IsNullOrWhiteSpace(TheNaughtyStrings.All[i + 1]))
+				{
+					continue;
+				}
+
+				var path = TheNaughtyStrings.All[i].Trim() + "/" + TheNaughtyStrings.All[i + 1];
+
+				ReadOnlySpan<char> span = path;
+
+				Range[] result = [.. span.SplitToPathSegments()];
+
+				Assert.That(result, Has.Length.EqualTo(2), $"Path: '{path}' of length {path.Length}");
+				Assert.Multiple(() =>
+				{
+					Assert.That(path[result[0]], Is.EqualTo(TheNaughtyStrings.All[i].Trim()));
+					Assert.That(path[result[1]], Is.EqualTo(TheNaughtyStrings.All[i + 1].Trim()));
+				});
+			}
 		}
 	}
 }
